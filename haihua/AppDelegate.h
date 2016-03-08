9b25cb92622322f5c60884b3db4f078c68c905fd
老黄 <by.huang@byhuangdemacbook-pro.local>
2016-03-08 16:11:58 +0800

    Initial Commit

---
+++ haihua/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  haihua
+//
+//  Created by by.huang on 16/3/8.
+//  Copyright © 2016年 by.huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
